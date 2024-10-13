eeeb8f6b38d63cb0298a87e25e004d3b3a302b4f
sohamshah2025 <146987811+sohamshah2025@users.noreply.github.com>
2024-10-12 20:35:56 -0500

    Fetch Data

--- react-392/src/App.jsx
+++ react-392/src/App.jsx
@@ -3,6 +3,8 @@ import logo from './logo.svg';
 //import './App.css';
 import Banner from './components/Banner';
 import CourseList from './components/CourseList';
+import { useJsonQuery } from './utilities/fetch';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const schedule = {
   "title": "CS Courses for 2018-2019",
@@ -34,20 +36,31 @@ const schedule = {
   }
 };
 
-const App = () => {
-  const [count, setCount] = useState(0);
+const Main = () => {
+  const [data, isLoading, error] = useJsonQuery('https://courses.cs.northwestern.edu/394/guides/data/cs-courses.php'); 
+
+  if (error) return <h1>Error loading course data: {`${error}`}</h1>;
+  if (isLoading) return <h1>Loading course data...</h1>;
+  if (!data) return <h1>No course data found</h1>;
+
+  const { title, courses } = data;
 
   return (
-    <div className="App">
-      <header className="App-header">
-      <div>
-        <Banner title={schedule.title} />
-        <CourseList courses={schedule.courses} />
-      </div>
-        
-      </header>
+    <div>
+      <Banner title={title} />       {/* Pass the fetched title to Banner */}
+      <CourseList courses={courses} /> {/* Pass the fetched courses to CourseList */}
     </div>
   );
 };
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <div className="container">
+      <Main />
+    </div>
+  </QueryClientProvider>
+);
 
 export default App;
